ccf3227a7eaa713438bd53ef56cb4a6c0aa8d09c
Nikita Litvin <deltaidea@derpy.ru>
2014-02-25 15:03:10 +0600

    refactor(safari): drop Safari support
    This closes #40.

--- build.bat
+++ build.bat
@@ -2,5 +2,4 @@
 copy source builds\chromium\scripts
 copy source builds\firefox\scripts
 copy source builds\maxthon\scripts
-copy source builds\safari\scripts
 copy source builds\opera\scripts
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 cp -r source/. builds/chromium/scripts
 cp -r source/. builds/firefox/scripts
 cp -r source/. builds/maxthon/scripts
-cp -r source/. builds/safari/scripts
 cp -r source/. builds/opera/scripts
