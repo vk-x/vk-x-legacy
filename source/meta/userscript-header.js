b4e09d75f31498bd4a69e650a50e6001d5551489
Nikita Litvin <deltaidea@derpy.ru>
2014-03-22 08:01:54 +0600

    fix(opera meta): allow to run on https://oauth.vk.com/*

--- source/meta/userscript-header.js
+++ source/meta/userscript-header.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @include http://vk.com/*
+// @include https://oauth.vk.com/*
 // @exclude http://vk.com/notifier.php*
 // @exclude http://vk.com/*_frame.php*
 // ==/UserScript==
