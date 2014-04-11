b1792f742adad54d7d234515590195c623e3a369
Nikita Litvin <deltaidea@derpy.ru>
2014-04-11 15:36:25 +0600

    fix(opera meta): add https urls to userscript header
    Add https:// versions of urls to userscript-header.js
    
    This closes #102.

--- source/meta/opera/userscript-header.js
+++ source/meta/opera/userscript-header.js
@@ -1,7 +1,11 @@
 // ==UserScript==
 // @include http://vk.com/*
+// @include https://vk.com/*
+// @include http://oauth.vk.com/*
 // @include https://oauth.vk.com/*
 // @exclude http://vk.com/notifier.php*
+// @exclude https://vk.com/notifier.php*
 // @exclude http://vk.com/*_frame.php*
+// @exclude https://vk.com/*_frame.php*
 // ==/UserScript==
 
